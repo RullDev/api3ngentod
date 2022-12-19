b36fcd2f3a5b4c09f2ed179cb232090dfafcf6fe
RullKhrl <khoirulmustofa767@gmail.com>
2022-12-19 15:06:59 +0700

    Update db.js

--- database/db.js
+++ database/db.js
@@ -3,9 +3,9 @@ var __path = process.cwd(),
      { color } = require(__path + '/lib/color.js')
 
 // Connection URL
-var url = 'https://api.sekha.me';
+var url = 'https://api.rull.me';
 try {
-if(url == 'https://api.sekha.me') throw console.log(color('Cek konfigurasi database, var url belum diisi','red'));
+if(url == 'https://api.rull.me') throw console.log(color('Cek konfigurasi database, var url belum diisi','red'));
 } catch (e) {
 	return;
 	}
